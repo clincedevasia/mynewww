9c05473e7cdb26735776ad1788b48ab03796d05a
clinceann <clincedevasia>
2018-04-14 11:42:33 +0530

    hh

--- ImageTransform.java
+++
@@ -1,8 +0,0 @@
-package com.statoil.reinvent.components.image;
-
-/**
- * Created by andreas on 13/01/16.
- */
-public class ImageTransform {
-    public static final String CAROUSEL = "carousel";
-}
